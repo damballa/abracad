58f52678098ebd6205614cacb4259e5c1eb06a5b
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2013-05-08 16:43:27 -0400

    Cover the rest of the reader.

--- src/clojure/cadavra/avro.clj
+++ src/clojure/cadavra/avro.clj
@@ -1,26 +1,27 @@
 (ns cadavra.avro
+  (:require [cadavra.util :refer [returning]])
   (:import [org.apache.avro Schema]
            [org.apache.avro.io DatumReader Decoder ResolvingDecoder]))
 
 (defn read-record
-  [^DatumReader this ^Schema expected ^ResolvingDecoder in]
+  [^DatumReader reader ^Schema expected ^ResolvingDecoder in]
   (let [rname (keyword (.getNamespace expected) (.getName expected))]
     (with-meta
       (persistent!
        (reduce (fn [m f]
                  (assoc! m
                    (keyword (.name f))
-                   (.read this nil (.schema f) in)))
+                   (.read reader nil (.schema f) in)))
                (transient {})
                (.readFieldOrder in)))
       {:type rname})))
 
 (defn read-enum
-  [^DatumReader this ^Schema expected ^Decoder in]
+  [^DatumReader reader ^Schema expected ^Decoder in]
   (keyword (-> expected .getEnumSymbols (.get (.readEnum in)))))
 
 (defn read-array
-  [^DatumReader this ^Schema expected ^ResolvingDecoder in]
+  [^DatumReader reader ^Schema expected ^ResolvingDecoder in]
   (let [vtype (.getElementType expected), n (.readArrayStart in)]
     (if-not (pos? n)
       []
@@ -28,11 +29,11 @@
        (loop [m (transient []), n (long n)]
          (if-not (pos? n)
            (let [n (.arrayNext in)] (if-not (pos? n) m (recur m n)))
-           (let [v (.read this nil vtype in)]
+           (let [v (.read reader nil vtype in)]
              (recur (conj! m v) (dec n)))))))))
 
 (defn read-map
-  [^DatumReader this ^Schema expected ^ResolvingDecoder in]
+  [^DatumReader reader ^Schema expected ^ResolvingDecoder in]
   (let [vtype (.getValueType expected), n (.readMapStart in)]
     (if-not (pos? n)
       {}
@@ -40,13 +41,15 @@
        (loop [m (transient {}), n (long n)]
          (if-not (pos? n)
            (let [n (.mapNext in)] (if-not (pos? n) m (recur m n)))
-           (let [k (.readString in), v (.read this nil vtype in)]
+           (let [k (.readString in), v (.read reader nil vtype in)]
              (recur (assoc! m k v) (dec n)))))))))
 
 (defn read-fixed
-  [^DatumReader this ^Schema expected ^Decoder in]
-  )
+  [^DatumReader reader ^Schema expected ^Decoder in]
+  (let [size (.getFixedSize expected), bytes (byte-array size)]
+    (returning bytes
+      (.readFixed in bytes 0 size))))
 
 (defn read-bytes
-  [^DatumReader this ^Schema expected ^Decoder in]
-  )
+  [^DatumReader reader ^Schema expected ^Decoder in]
+  (.readBytes in))
---
+++ src/clojure/cadavra/util.clj
@@ -0,0 +1,6 @@
+(ns cadavra.util)
+
+(defmacro returning
+  "Evaluates the result of expr, then evaluates the forms in body (presumably
+for side-effects), then returns the result of expr."
+  [expr & body] `(let [value# ~expr] ~@body value#))
