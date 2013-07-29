5bcc5bd741d45a282131805dac9431d690b4304c
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2013-07-29 05:11:12 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -7,4 +7,4 @@
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.avro/avro "1.7.4"]
-                 [cheshire/cheshire "5.1.1"]])
+                 [cheshire/cheshire "5.2.0"]])
