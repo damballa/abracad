8df53692325d85f969b6af154da247dd085d71d9
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2013-07-29 13:31:18 -0400

    Begin work on next patch iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.damballa/abracad "0.3.0"
+(defproject com.damballa/abracad "0.3.1-SNAPSHOT"
   :description "De/serialize Clojure data structures with Avro."
   :url "http://github.com/damballa/abracad"
   :license {:name "Eclipse Public License"
