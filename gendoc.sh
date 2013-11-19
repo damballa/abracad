32ec958c7f14ca950cf899a599bcfd0fd087408c
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2013-11-19 14:18:12 -0500

    De-over-automate gendoc script.

--- gendoc.sh
+++ gendoc.sh
@@ -1,12 +1,12 @@
 #! /bin/sh -e
 
-git checkout master
+#git checkout master
 lein doc
 git checkout gh-pages
 sleep 1
 find doc -type f -exec touch {} +
 rsync -r doc/ .
-git add .
-git commit
-git push -u damballa gh-pages
-git checkout master
+#git add .
+#git commit
+#git push -u damballa gh-pages
+#git checkout master
