6c995e141a2b49480d6d4e199d33e2c159e55bec
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2015-04-02 14:54:35 -0400

    Release version 0.4.12.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.damballa/abracad "0.4.12-SNAPSHOT"
+(defproject com.damballa/abracad "0.4.12"
   :description "De/serialize Clojure data structures with Avro."
   :url "http://github.com/damballa/abracad"
   :licenses [{:name "Eclipse Public License"
