7f9835bf6f3c46ef797900d80513857b5d2a4df7
Claynon Souza <claynon@gmail.com>
2018-02-22 11:08:58 +0100

    add coverage aand loc profiles

--- project.clj
+++ project.clj
@@ -1,26 +1,47 @@
-(defproject com.damballa/abracad "0.4.14-SNAPSHOT"
+(defproject nubank/abracad "0.4.15-SNAPSHOT"
   :description "De/serialize Clojure data structures with Avro."
   :url "http://github.com/damballa/abracad"
   :licenses [{:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
+              :url  "http://www.eclipse.org/legal/epl-v10.html"}
              {:name "Apache License, Version 2.0"
-              :url "http://www.apache.org/licenses/LICENSE-2.0.html"}]
+              :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}]
+
+  :plugins [[codox/codox "0.6.4"]
+            [lein-midje "3.2.1"]
+            [lein-cloverage "1.0.10"]
+            [lein-vanity "0.2.0"]]
+
+  :repositories [["central" {:url "https://repo1.maven.org/maven2/" :snapshots false}]
+                 ["clojars" {:url "https://clojars.org/repo/"}]]
+
   :global-vars {*warn-on-reflection* true}
-  :source-paths ["src/clojure"]
-  :java-source-paths ["src/java"]
-  :javac-options ["-target" "1.7" "-source" "1.7"]
+
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.avro/avro "1.8.0"]
+                 [org.apache.avro/avro "1.8.2"]
                  [cheshire/cheshire "5.6.1"]]
-  :plugins [[codox/codox "0.6.4"]]
+
+  :repl-options {:init-ns user}
+
   :codox {:include [abracad.avro abracad.avro.edn]}
+
   :aliases {"test-all" ["with-profile" ~(str "clojure-1-6:"
                                              "clojure-1-7:"
                                              "clojure-1-8")
-                        "test"]}
-  :profiles {:clojure-1-6 {:dependencies
+                        "test"]
+            "coverage" ["cloverage" "-s" "coverage"]
+            "loc"      ["vanity"]}
+
+  :profiles {:dev         {:dependencies
+                           [[midje "1.9.1" :exclusions [org.clojure/clojure]]]}
+             :clojure-1-6 {:dependencies
                            [[org.clojure/clojure "1.6.0"]]}
              :clojure-1-7 {:dependencies
                            [[org.clojure/clojure "1.7.0"]]}
              :clojure-1-8 {:dependencies
-                           [[org.clojure/clojure "1.8.0"]]}})
+                           [[org.clojure/clojure "1.8.0"]]}}
+
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
+  :test-paths ["test/"]
+
+  :javac-options ["-target" "1.7" "-source" "1.7"])
