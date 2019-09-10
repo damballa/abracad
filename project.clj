7a54ef7123459eacad7a43f6e784f498c8d8a64a
Łukasz Korecki <lukasz@korecki.me>
2019-09-10 02:52:51 +0000

    v0.4.16

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomnom/abracad "0.4.16-SNAPSHOT"
+(defproject nomnom/abracad "0.4.16"
   :description "De/serialize Clojure data structures with Avro"
   :url "http://github.com/nomnom/abracad"
   :licenses [{:name "Eclipse Public License"
