99c20a41dd428601d703c918dc89869fd1e25a33
Henry John Kupty <hkupty@users.noreply.github.com>
2019-10-10 16:57:35 +0200

    Bump dependencies (#14)
    * Bump dependencies
    
    This bumps avro from `1.8.2` to `1.9.1`.
    
    The bump is fix underlying CVEs on `org.apache.commons:commons-compress`:
    https://app.snyk.io/vuln/SNYK-JAVA-ORGAPACHECOMMONS-72275
    https://app.snyk.io/vuln/SNYK-JAVA-ORGAPACHECOMMONS-32473
    
    This also bumps other outdated dependency.
    
    Lastly, as this is a library, the clojure version is removed so newer clojure applications
    using it won't pull the old clojure version.
    
    * Add clojure-1.10 as a test target
    
    * Update test profiles' dependencies
    
    Newer avro version apparently doesn't bring `sappy` dependency along,
    so it's added so tests pass.
    
    Also, pre-`clojure-1.9` test profiles are kept with old matcher-combinators
    version because of `clojure.spec.alpha` dependency.
    
    * Bump version
    
    * Fix #13: Change project URL
    
    * Fix mismatch
    
    Co-Authored-By: Phillip Mates <phillip.mates@nubank.com.br>

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject nubank/abracad "0.4.16"
+(defproject nubank/abracad "0.4.17"
   :description "De/serialize Clojure data structures with Avro."
-  :url "http://github.com/damballa/abracad"
+  :url "http://github.com/nubank/abracad"
   :licenses [{:name "Eclipse Public License"
               :url  "http://www.eclipse.org/legal/epl-v10.html"}
              {:name "Apache License, Version 2.0"
               :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}]
 
-  :plugins [[codox/codox "0.6.4"]
+  :plugins [[codox/codox "0.10.7"]
             [lein-midje "3.2.1"]
-            [lein-cloverage "1.0.10"]
+            [lein-cloverage "1.1.2"]
             [lein-vanity "0.2.0"]]
 
   :repositories [["central" {:url "https://repo1.maven.org/maven2/" :snapshots false}]
@@ -16,34 +16,43 @@
 
   :global-vars {*warn-on-reflection* true}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.apache.avro/avro "1.8.2"]
-                 [cheshire/cheshire "5.6.1"]]
+  :dependencies [[org.apache.avro/avro "1.9.1"]
+                 [cheshire/cheshire "5.9.0"]]
 
   :codox {:include [abracad.avro abracad.avro.edn]}
 
   :aliases {"test-all" ["with-profile" ~(str "clojure-1-7:"
                                              "clojure-1-8:"
-                                             "clojure-1-9")
+                                             "clojure-1-9:"
+                                             "clojure-1-10")
                         "midje"]
             "coverage" ["cloverage" "-s" "coverage"]
             "loc"      ["vanity"]}
 
   :profiles {:dev         {:dependencies
-                           [[midje "1.9.1" :exclusions [org.clojure/clojure]]
-                            [nubank/matcher-combinators "0.2.1"]]}
+                           [[midje "1.9.9" :exclusions [org.clojure/clojure]]
+                            [org.xerial.snappy/snappy-java "1.1.7.3"]
+                            [nubank/matcher-combinators "1.2.1"]]}
              :clojure-1-7 {:dependencies
                            [[org.clojure/clojure "1.7.0"]
-                            [midje "1.9.1" :exclusions [org.clojure/clojure]]
+                            [midje "1.9.9" :exclusions [org.clojure/clojure]]
+                            [org.xerial.snappy/snappy-java "1.1.7.3"]
                             [nubank/matcher-combinators "0.2.1"]]}
              :clojure-1-8 {:dependencies
                            [[org.clojure/clojure "1.8.0"]
-                            [midje "1.9.1" :exclusions [org.clojure/clojure]]
+                            [midje "1.9.9" :exclusions [org.clojure/clojure]]
+                            [org.xerial.snappy/snappy-java "1.1.7.3"]
                             [nubank/matcher-combinators "0.2.1"]]}
              :clojure-1-9 {:dependencies
                            [[org.clojure/clojure "1.9.0"]
-                            [midje "1.9.1" :exclusions [org.clojure/clojure]]
-                            [nubank/matcher-combinators "0.2.1"]]}}
+                            [midje "1.9.9" :exclusions [org.clojure/clojure]]
+                            [org.xerial.snappy/snappy-java "1.1.7.3"]
+                            [nubank/matcher-combinators "1.2.1"]]}
+             :clojure-1-10 {:dependencies
+                           [[org.clojure/clojure "1.10.1"]
+                            [midje "1.9.9" :exclusions [org.clojure/clojure]]
+                            [org.xerial.snappy/snappy-java "1.1.7.3"]
+                            [nubank/matcher-combinators "1.2.1"]]}}
 
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
